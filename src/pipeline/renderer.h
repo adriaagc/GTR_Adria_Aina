123b24940d53d605963d1254bd3b7dd8050c240a
adriaagc <adria.garcia05@estudiant.upf.edu>
2026-04-07 20:13:28 +0200

    framework

---
+++ src/pipeline/renderer.h
@@ -0,0 +1,67 @@
+#pragma once // ensures the header file is included only once during compilation, preventing duplicate definitions.
+#include "scene.h"
+#include "prefab.h"
+
+#include "light.h"
+
+//forward declarations
+class Camera;
+class Skeleton;
+namespace GFX {
+	class Shader;
+	class Mesh;
+	class FBO;
+}
+
+namespace SCN {
+
+	class Prefab;
+	class Material;
+
+	// This class is in charge of rendering anything in our system.
+	// Separating the render from anything else makes the code cleaner
+	class Renderer
+	{
+	public:
+
+		struct sRenderable {
+			GFX::Mesh* mesh = nullptr; //the thing we want to render
+			Material* material = nullptr;
+			Matrix44 model; //where we want to render it
+		};
+
+		std::vector<sRenderable> render_list; // collect everything to render and render it in a loop
+
+		bool render_wireframe;
+		bool render_boundaries;
+
+		GFX::Texture* skybox_cubemap;
+
+		SCN::Scene* scene;
+
+		//updated every frame
+		Renderer(const char* shaders_atlas_filename );
+
+		//just to be sure we have everything ready for the rendering
+		void setupScene();
+
+		//add here your functions
+		//...
+
+		void parseNode(Node* node);
+
+		void parseSceneEntities(SCN::Scene* scene, Camera* camera);
+
+		//renders several elements of the scene
+		void renderScene(SCN::Scene* scene, Camera* camera);
+
+		//render the skybox
+		void renderSkybox(GFX::Texture* cubemap);
+
+		//to render one mesh given its material and transformation matrix
+		void renderMeshWithMaterial(const Matrix44 model, GFX::Mesh* mesh, SCN::Material* material);
+
+		void showUI();
+	};
+
+};
